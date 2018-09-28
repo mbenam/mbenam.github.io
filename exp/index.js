a4cb40891cbf62b221a5af0a5533f4dcaa3316ae
mbenam <badreenam@yahoo.com>
2018-09-28 13:05:46 -0500

    Add files via upload

---
+++ exp/index.js
@@ -0,0 +1,84 @@
+expense_url = 'https://api.airtable.com/v0/appXiTCoSdTmd4bDY/Expenses?fields[]=Amount&fields[]=Date';
+
+authorization = {headers: { Authorization: "Bearer keypLu3etR0QPhcYf" }};
+
+months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+monthlyExp = [];
+datasets = [];
+
+properties.forEach((value)=>{
+  expense_url = expense_url + '&fields[]=' + value;
+});
+
+
+const getExpense = async () => {
+    const response = await fetch(expense_url, authorization);
+    const json = await response.json();
+    temp = JSON.stringify(json).slice(11,-1);
+    expense = JSON.parse(temp);
+};  
+  
+const getMonthlyExp = async() => {
+  properties.forEach(()=> monthlyExp.push([0,0,0,0,0,0,0,0,0,0,0,0]));
+  expense.forEach((exp_record)=>{
+  exp_date = exp_record.fields.Date;
+  properties.forEach((property,index)=>{
+  
+    property_exp = exp_record.fields[property];
+    if(property_exp != 0){
+       month = Number(exp_date.slice(5,-3))-1;
+       monthlyExp[index][month] = monthlyExp[index][month] + property_exp;
+    }
+    
+  });
+  
+});
+  
+};
+
+
+const createDataset = async() => {
+  properties.forEach((property, index)=>{
+  
+  datasets.push({data:monthlyExp[index],
+          label:properties[index],
+          borderColor:colors[index],
+          fill:false
+         });
+  
+     });
+};
+
+const createChart = async() => {
+  
+  linedata = {type:'line',
+           data:{
+                  labels:months,
+                  datasets:datasets,
+                },
+           options:{
+             title: {
+                      display: true,
+                      text: 'Expense by Month'
+                    }
+                   }
+          
+          };
+
+  new Chart(document.getElementById("line-chart"),linedata);
+  
+};
+
+
+
+const main = async () => {
+
+  await getExpense();
+  await getMonthlyExp();
+  await createDataset();
+  await createChart();
+
+}
+
+main();
---
+++ exp/property.js
@@ -0,0 +1,3 @@
+properties = ['928SP','744SP','8426BF','4758TL'];
+
+colors = ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850'];
